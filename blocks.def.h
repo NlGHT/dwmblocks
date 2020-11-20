68d1efd5be33a6d7e8c18e3cdc9e75c9daba5ae0
NlGHT <tnight17@student.aau.dk>
2020-11-20 13:24:05 +0100

    Added brightness to status bar

--- blocks.def.h
+++ blocks.def.h
@@ -3,9 +3,10 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{" ♫ ",        "amixer get Master | awk -F'[][]' 'END{ print $2 }'",       0,   12},
+	{"☼ ",         "light | cut -d . -f 1 | sed s/$/%/",                       0,   11},
 	{"Mem: ",      "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",  30,  0},
 	{"Battery: ",  "sed 's/.*/&%/' /sys/class/power_supply/BAT0/capacity",     10,  0},
-	{"",           "date '+%b %d (%a) %R'",                               5,   0},
+	{"",           "date '+%b %d (%a) %R'",                                    5,   0},
 	// {"",           "date '+%b %d (%a) %I:%M%p'",                               5,   0},
 	// {"🔌",      "sed 's/.*/&%/' /sys/class/power_supply/BAT0/capacity",                10,  0},
 	// {"♫🔊🎧",      "amixer get Master | awk -F'[][]' 'END{ print $2 }'",  0,   12},
